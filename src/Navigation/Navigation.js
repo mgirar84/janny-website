6cf7c6635dca97ab6d2c0e93b7a084b8ea9b06a7
Joanne Girardet <milogirardet@gmail.com>
2020-08-28 11:27:48 +0100

    nav bar responsiveness WIP

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -4,20 +4,23 @@ import {Link} from 'react-router-dom'
 
 class Navigation extends React.Component {
 
-    myFunction() {
-        var x = document.getElementsByClassName("buttonsContainer");
-        if (x.id === "topNav") {
-          x.id += " responsive";
-        } else {
-          x.id = "topNav";
-        }
-    }
+    // navBarFunction () {
+    //     var x = document.getElementsByClassName("dropdown")
+    //     if (x.classname === "dropdown") {
+    //         x.classname += " responsive";
+    //     } else {
+    //         x.classname = "dropdown";
+    //     }
+    // }
 
 	render() {
         return (
-            <div class="buttonsContainer" id="topnav">
+            <div class="buttonsContainer">
+                <a href="javascript:void(0);" class="icon">
+                    <i class="fa fa-bars"></i>
+                </a>
                 <div class="dropdown">
-                    <button class="dropbtn" id="topnav">Articles</button>
+                    <button class="dropbtn">Articles</button>
                     <div class="dropdownContent">
                         <Link to="/articles/green-planet"><p>Green Planet</p></Link>
                         <Link to="/articles/cities"><p>Sustainable Cities</p></Link>
@@ -25,7 +28,7 @@ class Navigation extends React.Component {
                     </div>
                 </div>
                 <div class="dropdown">
-                    <button class="dropbtn" id="topnav">Poems</button>
+                    <button class="dropbtn">Poems</button>
                     <div class="dropdownContent">
                         <Link to="/poems/poem1"><p>Poem1</p></Link>
                         <Link to="/poems/poem2"><p>Poem2</p></Link>
@@ -33,14 +36,14 @@ class Navigation extends React.Component {
                     </div>
                 </div>
                 <div class="dropdown">
-                    <button class="dropbtn" id="topnav">Lectures</button>
+                    <button class="dropbtn">Lectures</button>
                     <div class="dropdownContent">
                         <Link to="/lectures/lecture1"><p>Lecture1</p></Link>
                         <Link to="/lectures/lecture2"><p>Lecture2</p></Link>
                     </div>
                 </div>
                 <div class="dropdown">
-                    <Link to="/books"><button class="dropbtn nonListLink" id="topnav">Books</button></Link>
+                    <Link to="/books"><button class="dropbtn nonListLink">Books</button></Link>
                     {/* <div class="dropdownContent">
                         <Link to="/books/Earthrise"><p>Earthrise</p></Link>
                         <Link to="/books/book2"><p>Book2</p></Link>
@@ -48,16 +51,13 @@ class Navigation extends React.Component {
                     </div> */}
                 </div>
                 <div class="dropdown">
-                    <button class="dropbtn" id="topnav">Documentaries</button>
+                    <button class="dropbtn">Documentaries</button>
                     <div class="dropdownContent">
                         <Link to="/documentaries/jungle-pharmacy"><p>Jungle Pharmacy</p></Link>
                         <Link to="/documentaries/doc2"><p>Doc2</p></Link>
                         <Link to="/documentaries/doc3"><p>Doc3</p></Link>
                     </div>
                 </div>
-                <a href="javascript:void(0);" class="icon" id="topnav" onclick={this.myFunction()}>
-                    <i class="fa fa-bars"></i>
-                </a>
             </div>
         )
 	}
