48498233e3626d8fa711ad89f3984a207ade96a3
Milo Girardet <milogirardet@milos-mbp.home>
2020-10-04 17:21:27 +0100

    added banner to about me, slight styling tweaks and added functions to hide drop downs on click

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -19,30 +19,30 @@ class Navigation extends React.Component {
                     <div className="dropdown">
                         <button className="dropbtn">Articles</button>
                         <div className="dropdownContent">
-                            <Link to="/articles/green-planet"><p>Green Planet</p></Link>
-                            <Link to="/articles/cities"><p>Sustainable Cities</p></Link>
-                            <Link to="/articles/energy"><p>Energy and Economics</p></Link>
+                            <Link to="/articles/green-planet" onClick={this.navBarFunction}><p>Green Planet</p></Link>
+                            <Link to="/articles/cities" onClick={this.navBarFunction}><p>Sustainable Cities</p></Link>
+                            <Link to="/articles/energy" onClick={this.navBarFunction}><p>Energy and Economics</p></Link>
                         </div>
                     </div>
                     <div className="dropdown">
-                        <Link to="/poems"><button className="dropbtn nonListLink">Poems</button></Link>
+                        <Link to="/poems" onClick={this.navBarFunction}><button className="dropbtn nonListLink">Poems</button></Link>
                     </div>
                     <div className="dropdown">
                         <button className="dropbtn">Lectures</button>
                         <div className="dropdownContent">
-                            <Link to="/lectures/lecture1"><p>Lecture1</p></Link>
-                            <Link to="/lectures/lecture2"><p>Lecture2</p></Link>
+                            <Link to="/lectures/lecture1" onClick={this.navBarFunction}><p>Lecture1</p></Link>
+                            <Link to="/lectures/lecture2" onClick={this.navBarFunction}><p>Lecture2</p></Link>
                         </div>
                     </div>
                     <div className="dropdown">
-                        <Link to="/books"><button className="dropbtn nonListLink">Books</button></Link>
+                        <Link to="/books" onClick={this.navBarFunction}><button className="dropbtn nonListLink">Books</button></Link>
                     </div>
                     <div className="dropdown">
                         <button className="dropbtn">Documentaries</button>
                         <div className="dropdownContent">
-                            <Link to="/documentaries/jungle-pharmacy"><p>Jungle Pharmacy</p></Link>
-                            <Link to="/documentaries/doc2"><p>Doc2</p></Link>
-                            <Link to="/documentaries/doc3"><p>Doc3</p></Link>
+                            <Link to="/documentaries/jungle-pharmacy" onClick={this.navBarFunction}><p>Jungle Pharmacy</p></Link>
+                            <Link to="/documentaries/doc2" onClick={this.navBarFunction}><p>Doc2</p></Link>
+                            <Link to="/documentaries/doc3" onClick={this.navBarFunction}><p>Doc3</p></Link>
                         </div>
                     </div>
                 </div>
