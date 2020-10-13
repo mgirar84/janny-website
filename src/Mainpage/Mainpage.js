e0b62a0d57972d5c839442de98331da218690160
Milo Girardet <milogirardet@milos-mbp.home>
2020-10-13 10:52:17 +0100

    added extra about me

--- src/Mainpage/Mainpage.js
+++ src/Mainpage/Mainpage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Mainpage.scss';
 
 class Mainpage extends React.Component {
-	render() {
+    render() {
         return (
             <div className="mainPageContainer">
                 <div className="aboutBanner"></div>
@@ -15,7 +15,7 @@ class Mainpage extends React.Component {
                     the interaction between cities, national economies and the global environment. Most of
                     his books have been published in various foreign language editions.
                     <br></br>
-                    <br></br>
+                        <br></br>
                     He is a recipient of a UN 'Global 500 Award for Outstanding Environmental
                     Achievements', and an Honorary Fellow of the Royal Institute of British Architects. From
                     2005 to 2010 he was Director of Programmes of the World Future Council (WFC). He is
@@ -24,70 +24,117 @@ class Mainpage extends React.Component {
                     He is married with two grown-up sons, and lives with his wife Barbara in Tintern,
                     Monmouthshire, UK.
                     </p>
-                    </div>
-                    <h4>Appointments:</h4>
-                    <ul>
-                        <li>1977-1992: Adviser, Town and Country Planning Association, London</li>
-                        <li>1980 - 2010: Associate Editor, Resurgence Magazine</li>
-                        <li>1987-89: Environment Consultant, Channel 4, London</li>
-                        <li>1990 - : Patron, The Soil Association, Bristol</li>
-                        <li>1994 - 2008: Chairman, The Schumacher Society, UK</li>
-                        <li>1995-1996: Consultant to the UN Cities Summit, Istanbul</li>
-                        <li>1995 - 2002: Co-founder and trustee, The Sustainable London Trust</li>
-                        <li>1995 - 2003: Visiting Professor, environmental planning, Middlesex University, London</li>
-                        <li>1995 - 2005: Trustee, The Earth Love Fund</li>
-                        <li>1998: Co-author, Sustainable London Study for the Bridge House Estate Fund</li>
-                        <li>1999 - 2006: Commissioning Editor, Schumacher Briefings, on aspects of sustainable
+                </div>
+                <h4>Appointments:</h4>
+                <ul>
+                    <li>1977-1992: Adviser, Town and Country Planning Association, London</li>
+                    <li>1980 - 2010: Associate Editor, Resurgence Magazine</li>
+                    <li>1987-89: Environment Consultant, Channel 4, London</li>
+                    <li>1990 - : Patron, The Soil Association, Bristol</li>
+                    <li>1994 - 2008: Chairman, The Schumacher Society, UK</li>
+                    <li>1995-1996: Consultant to the UN Cities Summit, Istanbul</li>
+                    <li>1995 - 2002: Co-founder and trustee, The Sustainable London Trust</li>
+                    <li>1995 - 2003: Visiting Professor, environmental planning, Middlesex University, London</li>
+                    <li>1995 - 2005: Trustee, The Earth Love Fund</li>
+                    <li>1998: Co-author, Sustainable London Study for the Bridge House Estate Fund</li>
+                    <li>1999 - 2006: Commissioning Editor, Schumacher Briefings, on aspects of sustainable
                         development (18 editions)</li>
-                        <li>2000: Curator, London Living City Exhibition, May to July 2000, RIBA, London</li>
-                        <li>2001 - 02: Consultant to Greater London Authority, sustainable resource management</li>
-                        <li>2003, May to July: Thinker in Residence, Adelaide, tasked with developing sustainability
+                    <li>2000: Curator, London Living City Exhibition, May to July 2000, RIBA, London</li>
+                    <li>2001 - 02: Consultant to Greater London Authority, sustainable resource management</li>
+                    <li>2003, May to July: Thinker in Residence, Adelaide, tasked with developing sustainability
                         strategies for South Australia that have since been implemented in full</li>
-                        <li>2003 - 2005: Visiting professor, urban planning, University of Northumbria</li>
-                        <li>2005 - : Cofounder and honorary member, World Future Council</li>
-                        <li>2006 - : Visiting professor, University of the West of England</li>
-                        <li>2006 - : Trustee, Artists Project Earth</li>
-                        <li>2009 - 2012: Senior consultant, Saudi Sustainability Initiative, Riyadh</li>
-                        <li>2010: - Member, World Academy of Arts and Science</li>
-                        <li>2011 - : Adviser, Ecological Sequestration Trust</li>
-                        <li>2016 - : Executive Committee Member of the Club of Rome</li>
-                    </ul>
-                    <h4>Books and reports:</h4>
-                    <ul>
-                        <li>1986: Far from Paradise - the story of human impact on the planet; BBC Publications,
+                    <li>2003 - 2005: Visiting professor, urban planning, University of Northumbria</li>
+                    <li>2005 - : Cofounder and honorary member, World Future Council</li>
+                    <li>2006 - : Visiting professor, University of the West of England</li>
+                    <li>2006 - : Trustee, Artists Project Earth</li>
+                    <li>2009 - 2012: Senior consultant, Saudi Sustainability Initiative, Riyadh</li>
+                    <li>2010: - Member, World Academy of Arts and Science</li>
+                    <li>2011 - : Adviser, Ecological Sequestration Trust</li>
+                    <li>2016 - : Executive Committee Member of the Club of Rome</li>
+                </ul>
+                <h4>Books and Reports:</h4>
+                <ul>
+                    <li>1986: Far from Paradise - the story of human impact on the planet; BBC Publications,
                         London, four foreign editions; (co-author)</li>
-                        <li>1987: Blueprint for a Green Planet - how to take practical action to fight pollution;
+                    <li>1987: Blueprint for a Green Planet - how to take practical action to fight pollution;
                         Dorling Kindersley, London, 14 foreign editions; (co-author)</li>
-                        <li>1992: Earthrise - how we can heal our injured planet; Paladin Books, HarperCollins,
+                    <li>1992: Earthrise - how we can heal our injured planet; Paladin Books, HarperCollins,
                         London; published for the Rio Earth Summit; (author)</li>
-                        <li>1992: The Gaia Atlas of Cities - new directions for sustainable urban living, Gaia Books,
+                    <li>1992: The Gaia Atlas of Cities - new directions for sustainable urban living, Gaia Books,
                         London, four foreign editions; special edition for Habitat II, Istanbul, June 1996; (author)</li>
-                        <li>1995: Getting London in Shape for 2000; report for London First on how London could
+                    <li>1995: Getting London in Shape for 2000; report for London First on how London could
                         take practical measures for sustainable development; (author)</li>
-                        <li>1996: Making Cities Work - the role of local authorities in the urban environment;
+                    <li>1996: Making Cities Work - the role of local authorities in the urban environment;
                         Earthscan, London. Commissioned by UN for Habitat II, Istanbul, June 1996; (co-author)</li>
-                        <li>1996: Creating a Sustainable London, report for Sustainable London Trust on
+                    <li>1996: Creating a Sustainable London, report for Sustainable London Trust on
                         implementing sustainable development in London; (co-author)</li>
-                        <li>1999: Creating Sustainable Cities, Green Books, Dartington; (author)</li>
-                        <li>2001: Tall Buildings and Sustainability, report for Corporation of London; (co-author)</li>
-                        <li>2002: The Peoples Planet, book of the TV series, Japanese edition only; (co-author)</li>
-                        <li>2003: London’s Environmental Sector, report for the LDA; (co-author)</li>
-                        <li>2003: Creating a Sustainable Adelaide, report for Premier of South Australia; (author)</li>
-                        <li>2004: Cities, People, Planet - Creating Liveable and Sustainable Cities, John Wiley,
+                    <li>1999: Creating Sustainable Cities, Green Books, Dartington; (author)</li>
+                    <li>2001: Tall Buildings and Sustainability, report for Corporation of London; (co-author)</li>
+                    <li>2002: The Peoples Planet, book of the TV series, Japanese edition only; (co-author)</li>
+                    <li>2003: London’s Environmental Sector, report for the LDA; (co-author)</li>
+                    <li>2003: Creating a Sustainable Adelaide, report for Premier of South Australia; (author)</li>
+                    <li>2004: Cities, People, Planet - Creating Liveable and Sustainable Cities, John Wiley,
                         London, New York and Sydney; (author)</li>
-                        <li>2005: Shaping Our Future - Creating the World Future Council, Green Books (co-author)</li>
-                        <li>2006: Creating Dongtan Eco-City, for Arup Consultants, London; (co-author)</li>
-                        <li>2007: Surviving the Century - Climate Change and Other Global Challenges, Earthscan,
+                    <li>2005: Shaping Our Future - Creating the World Future Council, Green Books (co-author)</li>
+                    <li>2006: Creating Dongtan Eco-City, for Arup Consultants, London; (co-author)</li>
+                    <li>2007: Surviving the Century - Climate Change and Other Global Challenges, Earthscan,
                         London; (editor and co-author)</li>
-                        <li>2008: Cities, People, Planet - Cities and Climate Change, Second edition, John Wiley,
+                    <li>2008: Cities, People, Planet - Cities and Climate Change, Second edition, John Wiley,
                         London, New York and Sydney; (author)</li>
-                        <li>2009: A Renewable World - Energy, Ecology, Equality, Green Books; (co-author)</li>
-                        <li>2014: Creating Regenerative Cities, Routledge, Oxford & New York; (author)</li>
-                    </ul>
-                
+                    <li>2009: A Renewable World - Energy, Ecology, Equality, Green Books; (co-author)</li>
+                    <li>2014: Creating Regenerative Cities, Routledge, Oxford & New York; (author)</li>
+                </ul>
+                <h4>Contribution to books:</h4>
+                <ul>
+                    <li>1997: Cities for a Small Planet, Richard Rogers, Faber and Faber, London</li>
+                    <li>1999: Growing Food, Growing Cities, Nico Bakker et al, editors, DSE, Feldafing</li>
+                    <li>2000: Ethics and the Built Environment, Warwick Fox, editor, Routledge, Oxford</li>
+                    <li>2005: Continuous Productive Urban Landscapes, Andre Viljoen, editor, Elsevier NL</li>
+                    <li>2009: Norman Foster, Drawings and Debates, Ivory Press, Madrid</li>
+                    <li>2015: The Routledge Handbook of Planning for Health and Well-Being, Hugh Barton, et
+                        al, editors, Routledge, Oxford</li>
+                    <li>2017: Green Economy Reader, Stanislav Shmelev, editor, Springer, Berlin</li>
+                </ul>
+                <h4>TV work:</h4>
+                <p>50 documentaries on cultural/ ecological themes:</p>
+                <ul>
+                    <li>1983-1986: Far from Paradise, a seven-hour TV series on the history of human
+                    interactions and impacts on the Earth; BBC, NDR, ORF, (initiator and researcher)</li>
+                    <li>1988: Jungle Pharmacy, Channel 4; 50 min. documentary on medicinal plant knowledge
+                    of Amazonian Indians; 6 international prices; shown on TV in 70 countries; (producer)</li>
+                    <li>1989: The Altamira Gathering, 15 min. news special for Channel 4; (director-producer)</li>
+                    <li>1990: Halting the Fires, Channel 4, 50 min documentary on halting the destruction of
+                    the Amazon forest; 1 film festival prize; shown on TV in 15 countries; (writer-producer)</li>
+                    <li>1994: Metropolis, Channel 4, 50 min doc. on "London's metabolism"; (writer-producer)</li>
+                    <li>1996: Urban Best Practices, WTN, 20 min. documentary for Habitat II, 1996; (director)</li>
+                    <li>1997 - 1999: Deadline 2000, 28 three-minute TV films on environment and the new
+                    millennium. Four films shown every 100 days in 1998 and 1999; (producer/ director)</li>
+                    <li>1999 - 2000: The People's Planet, a six-hour TV series for CNN, Discovery Channel, Canal
+                    Plus, Paris, and NHK TV, Tokyo, on the state of the world, and initiatives to improve the
+                    relationship between people and planet; (series consultant)</li>
+                    <li>2001-2002: Can We Live Without Oil and Water Wars, two TV scripts for NHK TV, Tokyo</li>
+                </ul>
+                <h4>Articles and lectures:</h4>
+                <p>Since the 1980s he has written extensively in the national and international press,
+                    particularly in papers like Resurgence, The Ecologist, Green Futures, Urban Futures,
+                    Habitat Debate, The Guardian, The Independent and The Observer.
+                </p>
+                <p>He has delivered hundreds of lectures to diverse audiences in the UK, and at
+                    conferences in 70 cities in over 30 countries across the world:
+                </p>
+                <p>
+                London, Bristol, Bath, Oxford, Brighton, Newcastle, Leeds, Liverpool, Cardiff, Tintern,
+                Monmouth, Chepstow, Usk, Trelleck, Belfast, Dublin, Hamburg, Essen, Munich, Bonn,
+                Berlin, Potsdam, Zurich, Rome, Vienna, Salzburg, Neukirchen, Amsterdam, Delft,
+                Brussels, Paris, Versailles, Madrid, Barcelona, Majorca, Delphi, Copenhagen, Bodo, Alta,
+                Moscow, St. Petersburg, New York, Los Angeles, San Francisco, Vancouver, Adelaide,
+                Abu Dhabi, Dubai, Riyadh, Doha, Jeddah, Al Hofuf, Alexandria, Cairo, Astana, Tokyo,
+                Singapore, Hong Kong, Beijing, Shanghai, Tianjin, Delhi, Mumbai, Dehradun, Auroville,
+                Rio de Janeiro, Johannesburg, Pretoria, Durban, Nairobi, Accra.
+                </p>
             </div>
         )
-	}
+    }
 };
 
 export default Mainpage
