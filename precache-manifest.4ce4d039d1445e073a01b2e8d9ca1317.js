8370dee8ff826a0fec45becb4e9eb14fc9c52d10
Milo Girardet <57747754+mgirar84@users.noreply.github.com>
2020-10-22 05:22:59 -0700

    Update precache-manifest.4ce4d039d1445e073a01b2e8d9ca1317.js
    removed janny-website

--- precache-manifest.4ce4d039d1445e073a01b2e8d9ca1317.js
+++ precache-manifest.4ce4d039d1445e073a01b2e8d9ca1317.js
@@ -1,366 +1,366 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "bb2bbee1e6a8ac371514c9e0326678bb",
-    "url": "/janny-website/index.html"
+    "url": "/index.html"
   },
   {
     "revision": "f50393b51c18b1162393",
-    "url": "/janny-website/static/css/main.18ccdc98.chunk.css"
+    "url": "/static/css/main.18ccdc98.chunk.css"
   },
   {
     "revision": "86983742f7c3f9aeb23b",
-    "url": "/janny-website/static/js/2.215424d1.chunk.js"
+    "url": "/static/js/2.215424d1.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/janny-website/static/js/2.215424d1.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.215424d1.chunk.js.LICENSE.txt"
   },
   {
     "revision": "f50393b51c18b1162393",
-    "url": "/janny-website/static/js/main.d83dba49.chunk.js"
+    "url": "/static/js/main.d83dba49.chunk.js"
   },
   {
     "revision": "2dded5bacac7c7ef86b8",
-    "url": "/janny-website/static/js/runtime-main.ee68a95f.js"
+    "url": "/static/js/runtime-main.ee68a95f.js"
   },
   {
     "revision": "2656e7fd2e8c59b6d4fcc221a1fec8c6",
-    "url": "/janny-website/static/media/aRenewableWorld.2656e7fd.jpg"
+    "url": "/static/media/aRenewableWorld.2656e7fd.jpg"
   },
   {
     "revision": "10c9b0e4c0f3857029996ebbb4b042b3",
-    "url": "/janny-website/static/media/abbey.10c9b0e4.jpg"
+    "url": "/static/media/abbey.10c9b0e4.jpg"
   },
   {
     "revision": "5594366c12a8619f9d5597e158281632",
-    "url": "/janny-website/static/media/adelaide.5594366c.jpg"
+    "url": "/static/media/adelaide.5594366c.jpg"
   },
   {
     "revision": "21dd7f6bee6a81e40f2ce3b97b1f1128",
-    "url": "/janny-website/static/media/agropolisDiagram.21dd7f6b.jpg"
+    "url": "/static/media/agropolisDiagram.21dd7f6b.jpg"
   },
   {
     "revision": "557dc29a802ac428aa61d653742089e2",
-    "url": "/janny-website/static/media/amplified.557dc29a.jpg"
+    "url": "/static/media/amplified.557dc29a.jpg"
   },
   {
     "revision": "ecadf2289610d71c7a2e69520b58463c",
-    "url": "/janny-website/static/media/amplifiedBanner.ecadf228.jpg"
+    "url": "/static/media/amplifiedBanner.ecadf228.jpg"
   },
   {
     "revision": "44b2862e11aba49fb143ebd535322f53",
-    "url": "/janny-website/static/media/birdsEyeCityLights.44b2862e.jpg"
+    "url": "/static/media/birdsEyeCityLights.44b2862e.jpg"
   },
   {
     "revision": "a4cd467b5e6a2886442e55d3faddfe37",
-    "url": "/janny-website/static/media/birdsEyePark.a4cd467b.jpg"
+    "url": "/static/media/birdsEyePark.a4cd467b.jpg"
   },
   {
     "revision": "7406c940b11d4f7ca52901e39dd9124b",
-    "url": "/janny-website/static/media/birdsEyeView.7406c940.jpg"
+    "url": "/static/media/birdsEyeView.7406c940.jpg"
   },
   {
     "revision": "89800e84a74f619bd03a96be44a574f4",
-    "url": "/janny-website/static/media/blueprintForAGreenPlanet.89800e84.JPG"
+    "url": "/static/media/blueprintForAGreenPlanet.89800e84.JPG"
   },
   {
     "revision": "659f7fd6c8033617eb1d1e37557e0800",
-    "url": "/janny-website/static/media/books.659f7fd6.jpg"
+    "url": "/static/media/books.659f7fd6.jpg"
   },
   {
     "revision": "b47ef0bc09a559abf3b8015ca06725a0",
-    "url": "/janny-website/static/media/callToAction.b47ef0bc.jpg"
+    "url": "/static/media/callToAction.b47ef0bc.jpg"
   },
   {
     "revision": "5215a5942de00c8fb3d6fe3627b56cf2",
-    "url": "/janny-website/static/media/carbonFig.5215a594.png"
+    "url": "/static/media/carbonFig.5215a594.png"
   },
   {
     "revision": "fecedfb3a5b398590375456b3b4010ab",
-    "url": "/janny-website/static/media/cattle.fecedfb3.jpg"
+    "url": "/static/media/cattle.fecedfb3.jpg"
   },
   {
     "revision": "475a5cb2cd14da69ddda97e088fd5a29",
-    "url": "/janny-website/static/media/ceremony.475a5cb2.png"
+    "url": "/static/media/ceremony.475a5cb2.png"
   },
   {
     "revision": "f90c3f8f0637cf16d85878e7a7c9968f",
-    "url": "/janny-website/static/media/citiesPeoplePlanet.f90c3f8f.jpg"
+    "url": "/static/media/citiesPeoplePlanet.f90c3f8f.jpg"
   },
   {
     "revision": "5c6582b87d52a1b569dfb3116e00cb6e",
-    "url": "/janny-website/static/media/citiesPlanCard.5c6582b8.jpg"
+    "url": "/static/media/citiesPlanCard.5c6582b8.jpg"
   },
   {
     "revision": "978d5291101d09e3c1610b6a386ded4c",
-    "url": "/janny-website/static/media/cityCanal.978d5291.jpg"
+    "url": "/static/media/cityCanal.978d5291.jpg"
   },
   {
     "revision": "ee3813cad06bb7493f4e3a9721c0cead",
-    "url": "/janny-website/static/media/cityLookUp.ee3813ca.jpg"
+    "url": "/static/media/cityLookUp.ee3813ca.jpg"
   },
   {
     "revision": "9cfe24dd32def1779ac1e2b368fed5a0",
-    "url": "/janny-website/static/media/colourfulPark.9cfe24dd.jpg"
+    "url": "/static/media/colourfulPark.9cfe24dd.jpg"
   },
   {
     "revision": "f6e76971e059a9daf30e0aec25eaf7db",
-    "url": "/janny-website/static/media/creatingRegenerativeCities.f6e76971.bin"
+    "url": "/static/media/creatingRegenerativeCities.f6e76971.bin"
   },
   {
     "revision": "69db13c5b6f6d39b494267febe2a8376",
-    "url": "/janny-website/static/media/creatingSustainableCities.69db13c5.jpg"
+    "url": "/static/media/creatingSustainableCities.69db13c5.jpg"
   },
   {
     "revision": "95ee50ff30b5197e0d657f0bf3cd83be",
-    "url": "/janny-website/static/media/creatingWorldFutureCouncil.95ee50ff.jpg"
+    "url": "/static/media/creatingWorldFutureCouncil.95ee50ff.jpg"
   },
   {
     "revision": "3e03e778aa59a413eecd0baa97f27351",
-    "url": "/janny-website/static/media/cultureAndEcologyBanner.3e03e778.jpg"
+    "url": "/static/media/cultureAndEcologyBanner.3e03e778.jpg"
   },
   {
     "revision": "5509102b1689a0e41e2ecc6a365780ce",
-    "url": "/janny-website/static/media/cultureAndEcologyCard.5509102b.jpg"
+    "url": "/static/media/cultureAndEcologyCard.5509102b.jpg"
   },
   {
     "revision": "20edf7f743928bd896851ff56615009f",
-    "url": "/janny-website/static/media/cultureAndEcologyImage.20edf7f7.jpg"
+    "url": "/static/media/cultureAndEcologyImage.20edf7f7.jpg"
   },
   {
     "revision": "c3b5edc2867e01574771c84afbf23f8f",
-    "url": "/janny-website/static/media/desertWind.c3b5edc2.jpg"
+    "url": "/static/media/desertWind.c3b5edc2.jpg"
   },
   {
     "revision": "a5e3da2c1babbc0774bfdb613f268446",
-    "url": "/janny-website/static/media/documentariesBanner.a5e3da2c.jpg"
+    "url": "/static/media/documentariesBanner.a5e3da2c.jpg"
   },
   {
     "revision": "e967a30eda1dbeaa28aa2f2172d35f5f",
-    "url": "/janny-website/static/media/earthEmergencyBanner.e967a30e.jpg"
+    "url": "/static/media/earthEmergencyBanner.e967a30e.jpg"
   },
   {
     "revision": "a4045b179eb6ca2d1f6fced8ed12f2be",
-    "url": "/janny-website/static/media/earthrise.a4045b17.jpg"
+    "url": "/static/media/earthrise.a4045b17.jpg"
   },
   {
     "revision": "26a8f9116a15309e1707c61fcaa8efdd",
-    "url": "/janny-website/static/media/earthrise2.26a8f911.jpg"
+    "url": "/static/media/earthrise2.26a8f911.jpg"
   },
   {
     "revision": "8869f555e70047f8b4a71715f61f6ead",
-    "url": "/janny-website/static/media/ecology2.8869f555.jpg"
+    "url": "/static/media/ecology2.8869f555.jpg"
   },
   {
     "revision": "52b3ef551657ff296c3d6667ad9d62ce",
-    "url": "/janny-website/static/media/ecopolisDiagram.52b3ef55.jpg"
+    "url": "/static/media/ecopolisDiagram.52b3ef55.jpg"
   },
   {
     "revision": "862715a696e6540a3b2a4e3271741ff9",
-    "url": "/janny-website/static/media/energyBanner.862715a6.jpg"
+    "url": "/static/media/energyBanner.862715a6.jpg"
   },
   {
     "revision": "845e9ab7a586e3f461434b83e30eab5b",
-    "url": "/janny-website/static/media/farFromParadise.845e9ab7.jpg"
+    "url": "/static/media/farFromParadise.845e9ab7.jpg"
   },
   {
     "revision": "b6be559041488be425760d9943a849ee",
-    "url": "/janny-website/static/media/farmstead.b6be5590.jpg"
+    "url": "/static/media/farmstead.b6be5590.jpg"
   },
   {
     "revision": "a21ac273b58bb0de95c6b28f18e9eeff",
-    "url": "/janny-website/static/media/forestBanner.a21ac273.jpg"
+    "url": "/static/media/forestBanner.a21ac273.jpg"
   },
   {
     "revision": "49cc27cb9cca98fc44bad0f38f4865e0",
-    "url": "/janny-website/static/media/forestLandscape.49cc27cb.jpg"
+    "url": "/static/media/forestLandscape.49cc27cb.jpg"
   },
   {
     "revision": "a4c41ba503d11c3977b6a7e31030f025",
-    "url": "/janny-website/static/media/forestPortrait.a4c41ba5.jpg"
+    "url": "/static/media/forestPortrait.a4c41ba5.jpg"
   },
   {
     "revision": "718182fe8cc41ba37356658f27700259",
-    "url": "/janny-website/static/media/gaia.718182fe.jpg"
+    "url": "/static/media/gaia.718182fe.jpg"
   },
   {
     "revision": "9330643531f6f4e7809d078d77045ffc",
-    "url": "/janny-website/static/media/gaiaAtlasOfCities.93306435.JPG"
+    "url": "/static/media/gaiaAtlasOfCities.93306435.JPG"
   },
   {
     "revision": "24361e29ecdbd8b5492a88bc0c6631a9",
-    "url": "/janny-website/static/media/greenKids.24361e29.jpg"
+    "url": "/static/media/greenKids.24361e29.jpg"
   },
   {
     "revision": "c34162e386c8b1a4e1430781361a5831",
-    "url": "/janny-website/static/media/greenPlanet.c34162e3.jpg"
+    "url": "/static/media/greenPlanet.c34162e3.jpg"
   },
   {
     "revision": "d7b9796983efb43ce70cb7b2fbceeede",
-    "url": "/janny-website/static/media/greenPlanet2.d7b97969.jpg"
+    "url": "/static/media/greenPlanet2.d7b97969.jpg"
   },
   {
     "revision": "224eb72d4fd223f84bea2f0941fb8504",
-    "url": "/janny-website/static/media/headingImage.224eb72d.jpg"
+    "url": "/static/media/headingImage.224eb72d.jpg"
   },
   {
     "revision": "320c24705d46507dccac19765157ef6c",
-    "url": "/janny-website/static/media/herder.320c2470.jpg"
+    "url": "/static/media/herder.320c2470.jpg"
   },
   {
     "revision": "5e404f13c9ff842928a1b48ce73c8078",
-    "url": "/janny-website/static/media/junglePharmacy.5e404f13.jpg"
+    "url": "/static/media/junglePharmacy.5e404f13.jpg"
   },
   {
     "revision": "8837e5752513982309fee044577929ef",
-    "url": "/janny-website/static/media/landForThePeople.8837e575.JPG"
+    "url": "/static/media/landForThePeople.8837e575.JPG"
   },
   {
     "revision": "b11ebcbadb41ad37f1be3fb1c87b21aa",
-    "url": "/janny-website/static/media/landtoseaArticle.b11ebcba.jpg"
+    "url": "/static/media/landtoseaArticle.b11ebcba.jpg"
   },
   {
     "revision": "75ca7d3f3a9417f102bedc6783398702",
-    "url": "/janny-website/static/media/landtoseaBanner.75ca7d3f.jpg"
+    "url": "/static/media/landtoseaBanner.75ca7d3f.jpg"
   },
   {
     "revision": "8d59f1fbae3406c68b7eba6556fc3a6d",
-    "url": "/janny-website/static/media/landtoseaCard.8d59f1fb.jpg"
+    "url": "/static/media/landtoseaCard.8d59f1fb.jpg"
   },
   {
     "revision": "488692d0d0b408c5c7a63d76fde6501d",
-    "url": "/janny-website/static/media/makingCitiesWork.488692d0.jpeg"
+    "url": "/static/media/makingCitiesWork.488692d0.jpeg"
   },
   {
     "revision": "c66ebec699ed7eac3b1a2e5ed098bdaf",
-    "url": "/janny-website/static/media/metabolismBanner.c66ebec6.jpg"
+    "url": "/static/media/metabolismBanner.c66ebec6.jpg"
   },
   {
     "revision": "058668cf3311c4ec15c5e505eb3f6a40",
-    "url": "/janny-website/static/media/metabolismDiagram.058668cf.jpg"
+    "url": "/static/media/metabolismDiagram.058668cf.jpg"
   },
   {
     "revision": "06ff01c64d5d9a2acd5655ec583804f5",
-    "url": "/janny-website/static/media/mistyCity.06ff01c6.jpg"
+    "url": "/static/media/mistyCity.06ff01c6.jpg"
   },
   {
     "revision": "8d4589ca0625c375b6815588d786aea1",
-    "url": "/janny-website/static/media/nature-taking-revenge.8d4589ca.png"
+    "url": "/static/media/nature-taking-revenge.8d4589ca.png"
   },
   {
     "revision": "3924593155aa06242351ed32615134cc",
-    "url": "/janny-website/static/media/nhk.39245931.JPG"
+    "url": "/static/media/nhk.39245931.JPG"
   },
   {
     "revision": "41841ea1c3b9962d04d80484915bcade",
-    "url": "/janny-website/static/media/nightTrainArticle.41841ea1.jpg"
+    "url": "/static/media/nightTrainArticle.41841ea1.jpg"
   },
   {
     "revision": "5d0951c93a17be32a296536e385c9bc1",
-    "url": "/janny-website/static/media/nightTrainBanner.5d0951c9.jpg"
+    "url": "/static/media/nightTrainBanner.5d0951c9.jpg"
   },
   {
     "revision": "6b3380dfbaea9e95cda45a3bba302f66",
-    "url": "/janny-website/static/media/nightTrainCard.6b3380df.jpg"
+    "url": "/static/media/nightTrainCard.6b3380df.jpg"
   },
   {
     "revision": "066eee04e006c4d83d4216c80d155dfa",
-    "url": "/janny-website/static/media/patchworkTurbines.066eee04.jpg"
+    "url": "/static/media/patchworkTurbines.066eee04.jpg"
   },
   {
     "revision": "d06a3edbbda257946af8f8e21b6ef045",
-    "url": "/janny-website/static/media/petropolisDiagram2.d06a3edb.jpg"
+    "url": "/static/media/petropolisDiagram2.d06a3edb.jpg"
   },
   {
     "revision": "78fa44754aeccb6546746fc3d306adcc",
-    "url": "/janny-website/static/media/poems.78fa4475.jpg"
+    "url": "/static/media/poems.78fa4475.jpg"
   },
   {
     "revision": "cd4c3c7719090fdf8c265353d960f5f2",
-    "url": "/janny-website/static/media/powerlines.cd4c3c77.jpg"
+    "url": "/static/media/powerlines.cd4c3c77.jpg"
   },
   {
     "revision": "869321e77ac99e2871f3b2e201d6dcb0",
-    "url": "/janny-website/static/media/protest.869321e7.jpg"
+    "url": "/static/media/protest.869321e7.jpg"
   },
   {
     "revision": "e80ef68a68081513b5f8dfa35a48826b",
-    "url": "/janny-website/static/media/pygmyForestCamp.e80ef68a.jpg"
+    "url": "/static/media/pygmyForestCamp.e80ef68a.jpg"
   },
   {
     "revision": "611ba0bb1c222a03df0180a99bdab282",
-    "url": "/janny-website/static/media/reframingEconomicsBanner.611ba0bb.jpg"
+    "url": "/static/media/reframingEconomicsBanner.611ba0bb.jpg"
   },
   {
     "revision": "2d6581d899809ad0d01f70fcc708a520",
-    "url": "/janny-website/static/media/reframingEconomicsCard.2d6581d8.jpg"
+    "url": "/static/media/reframingEconomicsCard.2d6581d8.jpg"
   },
   {
     "revision": "acc524ed21733b71edb9abb5d30620cf",
-    "url": "/janny-website/static/media/reframingEconomicsImage.acc524ed.jpg"
+    "url": "/static/media/reframingEconomicsImage.acc524ed.jpg"
   },
   {
     "revision": "3ea4769667e514615d3c51d4dfae7292",
-    "url": "/janny-website/static/media/regenerativeBanner.3ea47696.jpg"
+    "url": "/static/media/regenerativeBanner.3ea47696.jpg"
   },
   {
     "revision": "4b87998a0bd73ea017e502814b8693e8",
-    "url": "/janny-website/static/media/riceHarvest.4b87998a.jpg"
+    "url": "/static/media/riceHarvest.4b87998a.jpg"
   },
   {
     "revision": "df17929af68065ed4dc55e3f5fb5e4ef",
-    "url": "/janny-website/static/media/rome.df17929a.jpg"
+    "url": "/static/media/rome.df17929a.jpg"
   },
   {
     "revision": "5641cb5f4b3fb6e21487b596b286591f",
-    "url": "/janny-website/static/media/soybeans.5641cb5f.png"
+    "url": "/static/media/soybeans.5641cb5f.png"
   },
   {
     "revision": "ffd842e306b1a8e23d4bd8f4810e9acb",
-    "url": "/janny-website/static/media/survivingTheCentury.ffd842e3.jpg"
+    "url": "/static/media/survivingTheCentury.ffd842e3.jpg"
   },
   {
     "revision": "88e455ea2589922d9b8fb49008037b29",
-    "url": "/janny-website/static/media/teslaPower.88e455ea.png"
+    "url": "/static/media/teslaPower.88e455ea.png"
   },
   {
     "revision": "0f55f8d3665ca15ac68fc5d9a15b1ce6",
-    "url": "/janny-website/static/media/theViewFromTheAmazon.0f55f8d3.png"
+    "url": "/static/media/theViewFromTheAmazon.0f55f8d3.png"
   },
   {
     "revision": "d4ddf537080d9ccd164bcd499594e93f",
-    "url": "/janny-website/static/media/town.d4ddf537.png"
+    "url": "/static/media/town.d4ddf537.png"
   },
   {
     "revision": "6c313a1680bc165429345f1ede1ecd69",
-    "url": "/janny-website/static/media/urbanAgricultureCardImage.6c313a16.bmp"
+    "url": "/static/media/urbanAgricultureCardImage.6c313a16.bmp"
   },
   {
     "revision": "3b8eed17bcfbcee7d881825e260f11f8",
-    "url": "/janny-website/static/media/urbanCardImage.3b8eed17.jpg"
+    "url": "/static/media/urbanCardImage.3b8eed17.jpg"
   },
   {
     "revision": "499a46c2914baf7873cd93764c4d0fe6",
-    "url": "/janny-website/static/media/urbanSystemsBanner.499a46c2.jpg"
+    "url": "/static/media/urbanSystemsBanner.499a46c2.jpg"
   },
   {
     "revision": "53055f0f0b9244cd83105de311ce59ff",
-    "url": "/janny-website/static/media/urbanSystemsImage.53055f0f.jpg"
+    "url": "/static/media/urbanSystemsImage.53055f0f.jpg"
   },
   {
     "revision": "38473c99cef829cf09de4033b5c52e14",
-    "url": "/janny-website/static/media/urbanTransitionsBanner.38473c99.jpg"
+    "url": "/static/media/urbanTransitionsBanner.38473c99.jpg"
   },
   {
     "revision": "9cf70293435c2b3152bf1ecd5300a30a",
-    "url": "/janny-website/static/media/urbanTransitionsCardImage.9cf70293.jpg"
+    "url": "/static/media/urbanTransitionsCardImage.9cf70293.jpg"
   },
   {
     "revision": "ca0830e30cfb46dcc7dcdc89d49cca7b",
-    "url": "/janny-website/static/media/urbanTransitionsImage.ca0830e3.jpg"
+    "url": "/static/media/urbanTransitionsImage.ca0830e3.jpg"
   },
   {
     "revision": "002d8a6543d8367af70af9c4fe5bbf56",
-    "url": "/janny-website/static/media/urbanWorld.002d8a65.png"
+    "url": "/static/media/urbanWorld.002d8a65.png"
   }
 ]);
